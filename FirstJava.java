803950972bd1827c43ce2a88afd5094c8c2f1ebe
Veera Brammam .K <95432559+brammam246@users.noreply.github.com>
2023-09-04 01:34:24 +0530

    Update FirstJava.java

--- FirstJava.java
+++ FirstJava.java
@@ -1,5 +1,8 @@
 package com.qascript;
 
+import io.github.bonigarcia.wdm.WebDriverManagaer;
+// import java.time.Duration;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -11,13 +14,16 @@ public class FirstTest {
 
     @Test
     public void OpenBrowser()  {
-        WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+        //to set up chromedriver
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        //System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
         options.addArguments("disable-gpu");
         driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
+        driver.git("https://www.tooplate.com/view/2137-barista-cafe")
         System.out.println("Title of the page is: " + driver.getTitle());
         Assert.assertTrue("Page title is not correct",driver.getTitle().equals("Google"));
     }
